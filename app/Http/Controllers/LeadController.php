0e3c78084fbc04484b9d69446448fa134eb48100
archana kumari <archanachandel>
2022-07-05 15:51:38 +0530

    remove extra space

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -82,7 +82,6 @@ class LeadController extends Controller
                     $allchannel="1,2,3,4,5";
                 }
                 $channls= explode(",", $allchannel);
-                //dd()
                 foreach($channls as $key=>$channls){
                     $id=$channls;
                     $channels=Channels::find($id);
@@ -143,30 +142,30 @@ class LeadController extends Controller
             if($validator->fails()) { 
                  return response()->json(['code'=>'302','error'=>$validator->errors()]);            
                 }
-        $lead = new Lead;
-        $lead->name = $request->name;
-        $email=$request->email;
-      
-        $lead->email = $request->email;
-        $lead->assignee = $request->assignee;
-        $lead->category_id = $request->category_id;
-        $lead->skype_id = $request->skype_id;
-        $lead->phone_number = $request->phone_number;
-        $lead->message = $request->message;
-        $lead->ip_address = $request->ip_address;
-        $lead->status = $request->status;
-        $lead->channel_id = $request->channel_id;
-        $lead->datetime = date('Y-m-d H:i:s');
-        $lead->assignee_name =  $request->assignee_name;
-        $lead->created_by=$user->name;
-        $lead->save();
-
-        $data=Lead::latest()->take(1)->first();
-        $lead_id=$data->id;
-        $data1['lead_id']=$lead_id;
-        $user = NotifyMsg::create($data1);
+            $lead = new Lead;
+            $lead->name = $request->name;
+            $email=$request->email;
         
-        return response()->json(['status'=>'Success','code'=>200, 'data'=>$lead]);
+            $lead->email = $request->email;
+            $lead->assignee = $request->assignee;
+            $lead->category_id = $request->category_id;
+            $lead->skype_id = $request->skype_id;
+            $lead->phone_number = $request->phone_number;
+            $lead->message = $request->message;
+            $lead->ip_address = $request->ip_address;
+            $lead->status = $request->status;
+            $lead->channel_id = $request->channel_id;
+            $lead->datetime = date('Y-m-d H:i:s');
+            $lead->assignee_name =  $request->assignee_name;
+            $lead->created_by=$user->name;
+            $lead->save();
+
+            $data=Lead::latest()->take(1)->first();
+            $lead_id=$data->id;
+            $data1['lead_id']=$lead_id;
+            $user = NotifyMsg::create($data1);
+            
+            return response()->json(['status'=>'Success','code'=>200, 'data'=>$lead]);
         }
         catch(\Exception $e){
             return response()->json(['status'=>'error','code'=>'500','meassage'=>$e->getmessage()]);
@@ -264,7 +263,6 @@ class LeadController extends Controller
 
     public function editUser(Request $request){
 
-        // return response()->json($request);
         try {
             $user= auth('sanctum')->user();
             $usercheck = auth('sanctum')->check();
@@ -283,9 +281,8 @@ class LeadController extends Controller
             if($validator->fails()) { 
                 return response()->json(['code'=>'302','error'=>$validator->errors()]);            
                }
-               $user = Auth::user();
+            $user = Auth::user();
             $data=$request->all();
-            //unset($user['password']);
             $user->name = $request->name;
             $user->email = $request->email;
             if($request->password){
@@ -303,7 +300,6 @@ class LeadController extends Controller
             if($request->channel_id ){
                 $user->channel_id  = $request->channel_id ;
             }
-          
             $image=[];
             if($request->file('image')){
             $path = $request->file('image')->store('image','public');
@@ -323,16 +319,16 @@ class LeadController extends Controller
     public function getUserdetail(Request $request)
     { 
         try{
-         $user= auth('sanctum')->user();
-         $usercheck = auth('sanctum')->check();
-         if($usercheck == ""){
-           return response()->json(['status'=>'error','code'=>'401','message'=>'User is not login']);    
-          }    
+            $user= auth('sanctum')->user();
+            $usercheck = auth('sanctum')->check();
+            if($usercheck == ""){
+            return response()->json(['status'=>'error','code'=>'401','message'=>'User is not login']);    
+            }    
            $channel_id = ""; 
            $name=$request->name;
            $role_id=$request->role_id;
            $channel=$request->channel_id;
-         if($user->role_id=="2" ||$user->role_id=="3"){
+           if($user->role_id=="2" ||$user->role_id=="3"){
             $complete=User::SELECT('channel_id')->where('id','=',$user->id)->get();
             //dd($complete[0]);
             $channel_id=str_replace('[','',$complete[0]->channel_id);
@@ -375,9 +371,6 @@ class LeadController extends Controller
             //->join('leads')->where('assignee ', '=', 'user.id')->count();
             // ->join('leads', 'leads.assignee ', '=', 'user.id')
             //   ->get(['users.*', 'leads.assignee ']);
-
-          
-          
         //   if($user->role_id=="1"){
         //   $complete1=user::all();
         //   }
@@ -409,7 +402,6 @@ class LeadController extends Controller
        
            //dd($detail);
           // if(is_null())
-         
             return response()->json(['status'=>'success', 'code'=>'200','data'=>$complete1]);
         }
         catch(Exception $e){
@@ -432,11 +424,9 @@ class LeadController extends Controller
                 $channel = json_decode($user->channel_id);
               } 
               $channel_id=$request->channel_id;
-
               $status=$request->status;
               $owner_id=$request->assignee;
-              //dd($user->role_id);
-            $data=Lead::when($channel, function ($query,$channel) { 
+              $data=Lead::when($channel, function ($query,$channel) { 
                 return $query->whereIn("channel_id",  $channel);
             })
             ->when($channel_id, function ($query,$channel_id){ 
